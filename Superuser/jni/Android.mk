385b65d0347de1ca0278d8bf804ea0af2c7c4a0d
Pierre-Hugues Husson <husson@archos.com>
2015-09-06 22:18:34 +0200

    ndk-buildable again (untested)
    Conflicts:
    	Superuser/jni/Android.mk
    	Superuser/jni/Application.mk
    	Superuser/jni/placeholder/placeholder.c
    	Superuser/jni/placeholder/policy.c

--- Superuser/jni/Android.mk
+++ Superuser/jni/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := sqlite3 libcutils libc
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/sqlite3
-LOCAL_SRC_FILES := su/su.c su/activity.c su/db.c su/utils.c su/daemon.c su/pts.c su/hacks.c
-LOCAL_CFLAGS := -std=gnu11
+LOCAL_STATIC_LIBRARIES := sqlite3 libcutils libc libselinux
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/sqlite3 $(LOCAL_PATH)/libselinux/include $(LOCAL_PATH)/libsepol/include
+LOCAL_SRC_FILES := su/su.c su/activity.c su/db.c su/utils.c su/daemon.c su/pts.c su/hacks.c sqlite3/sqlite3.c
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\" -std=gnu11
 include $(BUILD_EXECUTABLE)
 
 
@@ -27,3 +27,6 @@ LOCAL_MODULE := sqlite3
 LOCAL_SRC_FILES := sqlite3/sqlite3.c
 LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION
 include $(BUILD_STATIC_LIBRARY)
+
+include $(my_path)/libselinux/Android.mk
+include $(my_path)/libsepol/Android.mk
